dd8a2aa7822cd3a9785013b195271898d37355b7
Assi Mizrachi <assi.mizrachi@gmail.com>
2013-07-10 20:36:35 +0000

    Reading message from file

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
   var message = fs.readFileSync('index.html').toString();
   response.send(message);
